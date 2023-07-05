2f46f3d7e302ae13e9b89accfea900db0fd63406
Marcos Vinicius Prysthon Nascimento <marcosvn@motorola.com>
2023-07-05 16:10:31 -0300

    Completinh step 8: run tests on desired browser

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -2,7 +2,20 @@ from selenium import webdriver
 import pytest
 
 @pytest.fixture()
-def setup():
+def setup(browser):
     PATH = 'C:/Users/marcosvn/Documents/studies/ict/chromedriver.exe'
-    driver = webdriver.Chrome(PATH)
+
+    if browser == 'chrome':
+        driver = webdriver.Chrome(PATH)
+    elif browser == 'firefox':
+        driver = webdriver.Firefox(PATH)
+    else:  # Default browser
+        driver = webdriver.Chrome(PATH)
     return driver
+
+def pytest_addoption(parser):    # This will get the value from CLI
+    parser.addoption("--browser")
+
+@pytest.fixture()
+def browser(request):            # This will return the browser to the setup method   
+    return request.config.getoption('--browser')
